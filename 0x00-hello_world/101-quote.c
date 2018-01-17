db8fd6507b03fa9e1208e2ee6002cdf2a38e3d48
Austin <aharshbe@live.com>
2018-01-17 22:25:56 +0000

    trying to make 101 work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<errno.h>
 /**
  * main - Entry point
  *
@@ -6,6 +7,7 @@
  */
 int main(void)
 {
+	errno = 1;
 	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
