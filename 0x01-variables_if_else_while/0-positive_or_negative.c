83a3783ba7902a486e80900eb43d5f80e16d2386
Austin <aharshbe@live.com>
2018-01-18 22:01:35 +0000

    not sure what the problem could be, hope this helps

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
@@ -8,11 +9,15 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
+
 	if(n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -21,9 +26,10 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	else
+	else if(n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,36 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
  *
+ * void - no value
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n, a;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
+
 	a = n % 10;
-	if(a == 0)
-	{
-		printf("Last digit of %d is 0 and is 0\n", a);
-	}
-	else if(n < 6 && n != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
-	}
-	else if(n > 5)
+
+	if(a > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	}
+	else if(a == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, a);
+	}
+	else if(a < 6 && a != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 
 	return (0);
