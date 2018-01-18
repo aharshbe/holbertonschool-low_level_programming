67b7713437c84c35b357142860bed483a2635ca7
Austin <aharshbe@live.com>
2018-01-18 22:10:40 +0000

    fixing 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,31 @@
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
  int main(void){
 
-	 int i = 0;
+	 int i;
+	
 	 char c = 'a';
 
-	 for(; i < 52; i++){
+	 for(i = 0; i < 52; i++){
 
 		 if(i == 26)
 		 {
 			 c = 'A';
 		 }
+		
 		 putchar(c);
+		 
 		 c++;
 
 	 }
+	
 	 putchar('\n');
+	 
 	 return(0);
  }
