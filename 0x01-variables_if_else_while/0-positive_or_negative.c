677eba0783784781107d434ea093574415604db0
Austin <aharshbe@live.com>
2018-01-18 21:46:04 +0000

    fixing 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
+ *
+ * void - empty
  *
  * Return: Always 0 (Success)
  */
