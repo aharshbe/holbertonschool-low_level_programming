1d2ec6ff56b9f061af56c79ec490fabf13e156d7
Austin <aharshbe@live.com>
2018-01-18 19:08:42 +0000

    adding excercise 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,18 +12,19 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if(n == 0)
 	{
-		printf("is zero\n");
-
-	}else if(n < 0)
-	{
-		printf("is negative\n");
-
-	}else if(n > 0)
-	{
-		printf("is positive\n");
-
+		printf("and is 0\n");
 	}
+	else if(n < 6 && n != 0)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	else if(n > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+
 	return (0);
 }
