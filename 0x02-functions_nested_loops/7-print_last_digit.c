97bc8157f937c044186e6466877d8060ff796342
Austin <aharshbe@live.com>
2018-01-24 01:51:14 +0000

    final fix for seven

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,13 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		n = (n + (-n * 2)) % 10;
+		_putchar(n + '0');
 		return (n);
 	}
 	else
 	{
 		n = n % 10;
+		_putchar(n + '0');
 		return (n);
 	}
 }
