4954afa030e88fe1eb0abd3913ba9b4b7810177e
Austin <aharshbe@live.com>
2018-01-24 08:05:37 +0000

    adding changes to 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+/**
+ * main - Short description, single line
+ * Return: 0
+ */
 
 int main(void)
 {
 	int n = 1024;
 	int k = 0;
 
-	for (int i = 0; i < n; i++){
+	for (int i = 0; i < n; i++)
+	{
 		if (i % 5 == 0 || i % 3 == 0)
 		{
-			k+=i;
+			k += i;
 		}
 	}
 	printf("%d\n", k);
+	return (0);
 }
