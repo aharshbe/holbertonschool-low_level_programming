5688771bdad3d306588cc9f487c38bbbe322b036
Austin <aharshbe@live.com>
2018-01-24 04:48:59 +0000

    adding 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 /**
  * print_to_98 - Short description, single line
- * @n: Description of parameter x
-(* a blank line
-* Description: Longer description of the function)?
-(* section header: Section description)*
+ * @n: Description of parameter n
 * Return: Description of the returned value
 */
 void print_to_98(int n)
@@ -12,7 +9,19 @@ void print_to_98(int n)
 
 	int i, j;
 
-	for (i = n, j = n; i <= 98; i++, j++)
+	if (n > 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			printf("%d", j);
+			if (j != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+
+	for (i = n; i <= 98; i++)
 	{
 		printf("%d", i);
 		if (i != 98)
