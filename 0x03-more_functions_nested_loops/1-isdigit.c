9ee0b45a4b00c2e0c4984342cfc068254e37a337
Austin <aharshbe@live.com>
2018-01-25 21:08:55 +0000

    adding 1 to repo

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isdigit - Short description, single line
+ * @c: Description of parameter c
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <=57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -5,5 +5,6 @@
 int _putchar(char c);
 void astring(char *string);
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
