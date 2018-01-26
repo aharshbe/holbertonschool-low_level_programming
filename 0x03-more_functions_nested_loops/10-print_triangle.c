e9bd07fff1fe748ff7ef677e68556fe89e798f35
Austin <aharshbe@live.com>
2018-01-26 00:41:13 +0000

    adding 10

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * print_triangle - Short description, single line
+ * @size: Description of parameter c
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		printf("\n");
+	}
+
+	for (i = 1; i <= size; i++){
+		for (j = size; j > i; j--){
+			printf(" ");
+		}
+
+		for (k = 0; k < i; k++){
+			printf("#");
+		}
+
+		printf("\n");
+
+	}
+}
