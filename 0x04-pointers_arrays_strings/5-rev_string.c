a5808dd3622d49bc492e7e34b68e314385279b29
Austin Harshberger <aharshbe@live.com>
2018-01-30 11:26:11 -0800

    adding fix for 5

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,17 @@
 void rev_string(char *s)
 {
 
-	int i, r, f;
-	char tmp[10];
+	int i, r, f, size;
+
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+	char tmp[size];
 
 	for (i = 0, r = 8; i < 10; i++, r--)
 	{
 		tmp[i] = s[r];
-
 	}
 
 	for (f = 0; f < 10; f++)
