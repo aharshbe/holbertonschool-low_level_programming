22da4bdd0126e8d320462b841ea70ac419f5dc30
Austin Harshberger <aharshbe@live.com>
2018-01-30 11:17:26 -0800

    fixing 5

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -10,15 +10,12 @@ void rev_string(char *s)
 	int i, r, f;
 	char tmp[10];
 
-
-	/*To copy into tmp reversed*/
 	for (i = 0, r = 8; i < 10; i++, r--)
 	{
 		tmp[i] = s[r];
 
 	}
 
-	/*To copy back into s*/
 	for (f = 0; f < 10; f++)
 	{
 		s[f] = tmp[f];
