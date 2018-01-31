2a76ff64d38b75736b1341d52e8f30e4043aad75
Austin Harshberger <aharshbe@live.com>
2018-01-31 00:08:28 -0800

    adding 101

--- 0x04-pointers_arrays_strings/101-keygen.c
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <time.h>
-
+/**
+* main - print every other character
+* Return: 0
+*/
 int main(void)
 {
 	int n = 0;
+
 	srand(time(NULL));
-	n = rand() * 1000000;
+	n = rand() * 10000000;
 	return (n);
 }
