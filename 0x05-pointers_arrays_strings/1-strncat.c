4cfb907e08dd2cf1cf9dac2751a83e064244d273
Austin Harshberger <aharshbe@live.com>
2018-02-01 10:47:41 -0800

    fixing 0 betty error

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* _strncat - Short description, single line
+* @dest: Description of parameter n
+* @src: Description of parameter n
+* @n: Description of parameter n
+* Return: 0
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
