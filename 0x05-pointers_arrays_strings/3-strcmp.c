05af37f76179046b6b0925ccce44157ca08b018c
Austin Harshberger <aharshbe@live.com>
2018-02-01 11:25:00 -0800

    adding 4

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+* _strncmp - Short description, single line
+* @s1: Description of parameter n
+* @s2: Description of parameter n
+* Return: 0
+*/
+int _strcmp(char *s1, char *s2)
+{
+
+	int size = 0, size2 = 0, isSame = 0, i = 0;
+
+	while (s1[size] != '\0')
+	{
+		size++;
+	}
+
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+
+
+	for ( ; i < size + size2; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			isSame = 0;
+		}
+		else
+		{
+			isSame = 1;
+		}
+	}
+
+	return (isSame);
+
+}
