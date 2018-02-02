ee50f7e4253589d9724a282e8f9a21451952d0f6
Austin Harshberger <aharshbe@live.com>
2018-02-01 19:56:15 -0800

    adding 4

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,20 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
-* rev_array - Short description, single line
-* @s1: Description of parameter n
-* @s2: Description of parameter n
+* reverse_array - Short description, single line
+* @a: Description of parameter n
+* @n: Description of parameter n
 * Return: 0
 */
+void reverse_array(int *a, int n)
+{
+	int i = n - 1, j = 0, tmp = 0, tmp2 = 0;
+
+	for ( ; i > 0; i--, j++)
+	{
+		tmp = a[i];
+		tmp2 = a[j];
+		a[j] = tmp;
+		a[i] = tmp2;
+	}
+}
