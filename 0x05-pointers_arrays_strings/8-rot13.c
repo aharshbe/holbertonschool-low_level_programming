a84f2521c8460fe5685bbd39b77414ddb3682682
Austin Harshberger <aharshbe@live.com>
2018-02-02 02:23:46 -0800

    adding half working version of rot 13

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+* rot13 - Short description, single line
+* @s: Description of parameter n
+* Return: 0
+*/
+char *rot13(char *s)
+{
+
+	int i = 0, rot = 13;
+
+	for ( ; ((s[i] != '\0') && (s[i] >= 65 && s[i] <= 90)) || (s[i] >= 97 && s[i] <= 122); i++)
+    {
+        
+        if ((s[i] + rot) > 90 || (s[i] + rot) > 122)
+        {
+        	s[i] -= rot;
+        }
+        else if (s[i] + rot < 65 || s[i] + rot < 97)
+        {
+        	s[i] = s[i];
+        }
+        else
+        {
+        	s[i] += rot;
+        }
+    }
+	return (s);
+}
