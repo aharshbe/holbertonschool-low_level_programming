75194d84661dc15a5d6d8cd17510e59c2e686ff8
Austin Harshberger <aharshbe@live.com>
2018-02-01 19:36:48 -0800

    fixing strcmp?

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -7,32 +7,31 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-
 	int size = 0, size2 = 0, isSame = 0, i = 0;
 
 	while (s1[size] != '\0')
 	{
 		size++;
 	}
-
 	while (s2[size2] != '\0')
 	{
 		size2++;
 	}
 
-
-	for ( ; i < size + size2; i++)
+	if (size > size2)
+	{
+		return (1);
+	}
+	for ( ; i < size + size2 && s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
 		{
-			isSame = 0;
+			isSame = (0);
 		}
-		else
+		else if (s1[i] != s2[i])
 		{
-			isSame++;
+			return (-1);
 		}
 	}
-
 	return (isSame);
-
 }
