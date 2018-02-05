9fc0ed74c2096a2dfb2dc21f7686e665866e4dd7
Austin Harshberger <aharshbe@live.com>
2018-02-05 13:07:31 -0800

    adding initial 2

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* strspn - Short description, single line
+* @s: Description of parameter s
+* @accept: Description of parameter b
+* Return: 0
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
