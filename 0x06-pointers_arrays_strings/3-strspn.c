ea2a0f54ad2ba8f16080f36c802f2b2ae162f1d9
Austin Harshberger <aharshbe@live.com>
2018-02-05 15:41:16 -0800

    adding betty changes

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -11,20 +11,20 @@ unsigned int _strspn(char *s, char *accept)
 
 	int i = 0, j = 0;
 
- 	for (i = 0; ;  i++)
- 	{
- 		for (j = 0; accept[j] != '\0'; j++)
- 		{
- 			if (accept[j] == s[i])
- 			{
- 				num++;
- 				break;
- 			}
- 		}
- 		if (accept[j] == '\0')
- 		{
- 			break;
- 		}
- 	}
+	for (i = 0; ;  i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				num++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			break;
+		}
+	}
 	return (num);
 }
