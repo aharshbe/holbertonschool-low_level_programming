072bee9854e0bcc9e71ce46c83c323e2426f2973
Austin Harshberger <aharshbe@live.com>
2018-02-10 15:38:41 -0800

    adding number 4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,36 +1,32 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
-* main - Short description, single line
+* main - description
 * @argc: arg count
 * @argv: arg vector
 * Return: 0
 */
-int main(int argc, char **argv)
-{
-	int i = 1, result = 0;
 
-	if (argc > 1)
+int main(int argc, char *argv[])
+{
+	int i = 1, num = 0, j = 0;
+
+	for ( ; i < argc; i++)
 	{
-		while (i < argc)
+		if (isdigit(argv[i][0]))
 		{
-			if (argv[i][0] > 48 && argv[i][0] <= 57)
-			{
-				result += atoi(argv[i]);
-				i++;
-			}
-			else
-			{
-				printf("Error\n");
-				result = -1;
-				return (1);
-			}
+			num += atoi(argv[i]);
+			j++;
+
 		}
-		if (!(result < 0))
-			printf("%d\n", result);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
-	else
-	{
-		printf("%d\n", 0);
-	}
+	printf("%d\n", num);
+
+	return (0);
 }
---
+++ 0x09-argc_argv/ADD.C
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Short description, single line
+* @argc: arg count
+* @argv: arg vector
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	int i = 1, result = 0;
+
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			if (argv[i][0] > 48 && argv[i][0] <= 57)
+			{
+				result += atoi(argv[i]);
+				i++;
+			}
+			else
+			{
+				printf("Error\n");
+				result = -1;
+				return (1);
+			}
+		}
+		if (!(result < 0))
+			printf("%d\n", result);
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
+}
