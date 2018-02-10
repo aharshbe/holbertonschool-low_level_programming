c63b4d6f7a8c11dfbd643932ed47117058918b0d
Austin Harshberger <aharshbe@live.com>
2018-02-10 12:55:38 -0800

    adding 100 but it still goes over line count

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -1,9 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+* check_change - Short description, single line
+* @change: amount of change
+* Return: 0
+*/
+int check_change(int change)
+{
+	int coins = 0;
 
+	while (change >= 1)
+	{
+		if (change >= 25)
+		{
+			if (change % 25 == 0)
+			{
+				coins += change / 25;
+				break;
+			}
+			else
+			{
+				coins += change / 25;
+				change %= 25;
+			}
+		}
+		else if (change >= 10)
+		{
+			if (change % 10 == 0)
+			{
+				coins += change / 10;
+				break;
+			}
+			else
+			{
+				coins += change / 10;
+				change %= 10;
+			}
+		}
+		else if (change >= 5)
+		{
+			if (change % 5 == 0)
+			{
+				coins += change / 5;
+				break;
+			}
+			else
+			{
+				coins += change / 5;
+				change %= 5;
+			}
+		}
+		else if (change >= 2)
+		{
+			if (change % 2 == 0)
+			{
+				coins += change / 2;
+				break;
+			}
+			else
+			{
+				coins += change / 2;
+				change %= 2;
+			}
+		}
+		else if (change >= 1)
+		{
+			coins += 1;
+			change -= 1;
+		}
+	}
+	return (coins);
+}
+/**
+* main - Short description, single line
+* @argc: arg count
+* @argv: arg vector
+* Return: 0
+*/
 int main(int argc, char **argv)
 {
-	int change = 0, coins = 0;
+	int change = 0;
 
 	if (argc != 2)
 	{
@@ -13,72 +89,7 @@ int main(int argc, char **argv)
 	else
 	{
 		change = atoi(argv[1]);
-
-		while (change >= 1)
-		{
-			if (change >= 25)
-			{
-				if (change % 25 == 0)
-				{
-					coins += change / 25;
-					break;
-				}
-				else
-				{
-					coins += change / 25;
-					change %= 25;
-				}
-			}
-			else if (change >= 10)
-			{
-				if (change % 10 == 0)
-				{
-					coins += change / 10;
-					break;
-				}
-				else
-				{
-					coins += change / 10;
-					change %= 10;
-				}
-			}
-			else if (change >= 5)
-			{
-				if (change % 5 == 0)
-				{
-					coins += change / 5;
-					break;
-				}
-				else
-				{
-					coins += change / 5;
-					change %= 5;
-				}
-			}
-			else if (change >= 2)
-			{
-				if (change % 2 == 0)
-				{
-					coins += change / 2;
-					break;
-				}
-				else
-				{
-					coins += change / 2;
-					change %= 2;
-				}
-			}
-			else if (change >= 1)
-			{
-				coins += 1;
-				change -= 1;
-			}
-			else
-			{
-				printf("Don't deal.\n");
-			}	
-		}
-		printf("%d\n", coins);
+		printf("%d\n", check_change(change));
 	}
 	return (0);
 }
