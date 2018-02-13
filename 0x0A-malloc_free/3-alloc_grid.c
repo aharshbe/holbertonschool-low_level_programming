4fca0d4cfa6c7d358dcd7682d48871a64730335c
Austin Harshberger <aharshbe@live.com>
2018-02-13 15:51:55 -0800

    adding changes to re-add malloc

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -10,21 +10,28 @@ int **alloc_grid(int width, int height)
 {
 	int **p;
 
-	int i = 0;
+	int i = 0, j = 0;
 
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	p = (int **)calloc(width * height, width * sizeof(int *));
+	p = (int **)malloc(width * sizeof(int *));
 
 	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < width; i++)
-		p[i] = (int *)calloc(width * height, height * sizeof(int));
+		p[i] = (int *)malloc(height * sizeof(int));
 
 	if (p[0] == NULL)
 		return (NULL);
 
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
 	return (p);
 }
