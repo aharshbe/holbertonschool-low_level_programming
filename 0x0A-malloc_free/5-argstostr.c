e7c3ae53cd1bb0522fb1c92d90b550d33cc44532
Austin Harshberger <aharshbe@live.com>
2018-02-13 19:44:25 -0800

    adding intal commit for 5

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* argstostr - Short description, single line
+* @ac: Description of parameter s1
+* @av: Description of parameter s2
+* Return: 0
+*/
+char *argstostr(int ac, char **av)
+{
+	char *val;
+	int s_outer = 0, s_inner = 0, size = 0, argc = 1, argv = 0, i = 0;
+
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
+		return (NULL);
+
+	while (s_outer < ac)
+	{
+		while (av[s_outer][s_inner] != '\0')
+		{
+			size++;
+			s_inner++;
+		}
+		s_inner = 0;
+		s_outer++;
+	}
+	val = malloc(size);
+	while (argc < ac)
+	{
+		while (argv < size)
+		{
+			if (av[argc][argv] == '\0')
+			{
+				val[i] = '\n';
+			}
+			else
+			{
+				val[i] = av[argc][argv];
+			}
+			argv++;
+			i++;
+		}
+		argc++;
+	}
+	val[size] = '\n';
+	return (val);
+}
--- 0x0A-malloc_free/test.c
+++ 0x0A-malloc_free/test.c
@@ -1,37 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* alloc_grid - Short description, single line
-* @width: Description of parameter s1
-* @height: Description of parameter s2
+* argstostr - Short description, single line
+* @ac: Description of parameter s1
+* @av: Description of parameter s2
 * Return: 0
 */
-int **alloc_grid(int width, int height)
+char *argstostr(int ac, char **av)
 {
-	int **p;
+	int s_outer = 0, s_inner = 0, size = 0, i = 0, j = 0, freed = 0;
+	char **argv2;
 
-	int i = 0, j = 0;
-
-	if (width < 1 || height < 1)
+	if (ac == 0)
+		return (NULL);
+	if(av == NULL)
 		return (NULL);
 
-	p = (int **)calloc(width * height, width * sizeof(int *));
-
-	if (p == NULL)
-		return (NULL);
-
-	for (i = 0; i < width; i++)
-		p[i] = (int *)calloc(width * height, height * sizeof(int));
-
-	if (p[0] == NULL)
-		return (NULL);
-
-	for (i = 0; i < width; i++)
+	while (s_outer < ac)
 	{
-		for (j = 0; j < height; j++)
+		while(av[s_outer][s_inner] != '\0')
 		{
-			p[i][j] = 0;
+			size++;
+			s_inner++;
+		}
+		s_inner = 0;
+		s_outer++;
+	}
+
+	argv2 = malloc(ac);
+	
+	for (i = 0; i < ac; i++)
+	{
+		argv2[i] = malloc(size * sizeof(char *));
+		
+		if (argv2[i] == NULL)
+		{
+			while (freed <= i)
+			{
+				free(argv2[i]);
+				i--;
+			}
+			free(argv2);
+			return (NULL);
 		}
 	}
-	return (p);
+
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; j < 1; j++)
+		{
+			argv2[i] = av[i];
+			printf("%s\n", argv2[i]);
+		}
+	}
+
+	return (0);
 }
