f8be546da5e6e3763d500afd753bd76ef0725d44
Austin Harshberger <aharshbe@live.com>
2018-02-15 13:57:40 -0800

    adding calloc

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _calloc - Short description, single line
+* @nmemb: Description of parameter size
+* @size: Description of parameter size
+* Return: 0
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int elements = nmemb, memory_s = size, i = 0;
+	char *p;
+
+	if (nmemb == 0)
+		return (NULL);
+
+	if (size == 0)
+		return (NULL);
+
+
+	p = malloc(elements * memory_s);
+
+	if (p == NULL)
+		return (NULL);
+
+	for ( ; i < elements; i++)
+	{
+		p[i] = 0;
+	}
+
+	return (p);
+}
