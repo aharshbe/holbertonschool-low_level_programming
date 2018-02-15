2e756ec75471a1e0b2e4fcdff7a5ef48595c93be
Austin Harshberger <aharshbe@live.com>
2018-02-15 15:56:08 -0800

    seeing how much of advanced I passed

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _realloc - Short description, single line
+* @ptr: Description of parameter size
+* @old_size: Description of parameter size
+* @new_size: Description of parameter size
+* Return: 0
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int old_size_i = old_size, new_size_i = new_size, i = 0;
+	int actual_s = new_size - old_size;
+
+	char *new_p;
+	char *ptr_p = ptr;
+
+	if (new_size_i == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size_i == old_size_i)
+		return (ptr);
+
+	if (ptr == NULL)
+		new_p = malloc(new_size_i);
+	else
+		new_p = malloc(sizeof(char) * new_size_i);
+
+	if (new_p == NULL)
+		return (NULL);
+
+	while (i < actual_s)
+	{
+		new_p[i] = ptr_p[i];
+		i++;
+	}
+	return (new_p);
+}
