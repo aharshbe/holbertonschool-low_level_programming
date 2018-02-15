c4b348812e74d8f73a2f718a277d22ba376ad53b
Austin Harshberger <aharshbe@live.com>
2018-02-15 13:08:13 -0800

    adding betty fix

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* create_array - Short description, single line
+* malloc_checked - Short description, single line
 * @b: Description of parameter size
 * Return: 0
 */
