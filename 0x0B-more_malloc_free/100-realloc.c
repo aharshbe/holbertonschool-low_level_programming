183c3c2e046b305cfa0811ad4f6178b5290739bc
Austin Harshberger <aharshbe@live.com>
2018-02-15 16:36:37 -0800

    adding changes

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	if (ptr == NULL)
-		return(malloc(sizeof(char) * new_size_i));
+		return (malloc(sizeof(char) * new_size_i));
 
 	new_p = malloc(sizeof(char) * new_size_i);
 
