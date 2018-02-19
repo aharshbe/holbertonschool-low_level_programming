5a9b4f077ffa07609579efb09a396ed16458b9ef
Austin Harshberger <aharshbe@live.com>
2018-02-19 13:59:45 -0800

    adding null check fix

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -8,13 +8,14 @@
  * Description: Longer description
  */
 #include "dog.h"
+#include <stdlib.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (!(!d))
+	if (d != NULL)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
