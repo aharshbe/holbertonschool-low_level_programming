e1311cee780a0b7b94d2094243c971f090cbcb74
Austin Harshberger <aharshbe@live.com>
2018-02-19 15:52:09 -0800

    adding 5

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - check the code for Holberton School students.
+ * @d: - dog structure
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
