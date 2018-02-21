438b6b39545469ecae344932e50a39d814666022
Austin Harshberger <aharshbe@live.com>
2018-02-21 14:33:22 -0800

    adding check for null on name and null on fnction

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 
+
 /**
  * print_name - prints a name as is
  * @name: name of the person
@@ -11,12 +12,9 @@
 void print_name(char *name, void (*f)(char *))
 {
 
-	if (!name)
+	if (name != 0)
 	{
-
-	}
-	else
-	{
-		f(name);
+		if (f != 0)
+			f(name);
 	}
 }
