fb724d516b6f7604a2b90d89b60e8ae478bfdef7
Austin Harshberger <aharshbe@live.com>
2018-02-21 20:21:22 -0800

    adding 100

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints a name as is
+ * @argc: name of the person
+ * @argv: name of the person
+ *
+ * Return: 0 is success or 98 if fail.
+ */
+
+int main(int argc, char **argv)
+{
+	int total = 0, i = 0;
+
+	char *p;
+
+	if (argc > 0 && argc < 3)
+	{
+		p = (char *)&main;
+		total = atoi(argv[1]);
+
+		if (total < 0)
+		{
+			printf("Error\n");
+			return (2);
+		}
+
+		for ( ; i < total; i++)
+			printf("%02hhx ", p[i]);
+		printf("\n");
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
