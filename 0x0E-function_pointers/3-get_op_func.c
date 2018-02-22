3d14a52b3b4b43bceeb43a60bc47983a18d908bb
Austin Harshberger <aharshbe@live.com>
2018-02-21 19:13:34 -0800

    adding NULL check instead of sizeof

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -1,5 +1,5 @@
 #include "3-calc.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * get_op_func - prints a name as is
@@ -10,14 +10,12 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	int i = 0, size = 0;
+	int i = 0;
 
 	op_t ops[] = { {"+", op_add}, {"-", op_sub},
 	{"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL} };
 
-	size = sizeof(ops) / sizeof(op_t);
-
-	while (i < size)
+	while (ops[i].op != NULL)
 	{
 		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
