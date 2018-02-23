09fd8d944904c15b2e2c826a27c35b1009df6af9
Austin <aharshbe@live.com>
2018-02-23 12:12:50 -0800

    adding numner 1

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_numbers - prints a name as is
+ * @separator: name of the person
+ * @n: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = 0;
+
+	va_list p;
+	va_start(p, (int)n);
+
+	while (i < (int)n)
+	{
+		if (i == (int)n - 1)
+		{
+			printf("%d\n", va_arg(p, int));
+		}
+		else
+		{
+			printf("%d", va_arg(p, int));
+			if (separator != NULL)
+				printf("%s", separator);
+		}
+		i++;
+	}
+
+	va_end(p);
+}
