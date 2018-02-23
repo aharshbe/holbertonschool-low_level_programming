f9ddc7e1ae26743997776a5aa867b826db7e80b7
Austin <aharshbe@live.com>
2018-02-23 11:53:02 -0800

    adding 0 and main

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - prints a name as is
+ * @n: name of the person
+ *
+ * Return: Nothing.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	int i = 0, sum = 0;
+
+	va_list arg_p; 
+
+	va_start(arg_p, (int)n);
+
+	while (i < (int)n)
+	{
+		sum += va_arg(arg_p, int);
+		i++;
+	}
+
+	va_end(arg_p);
+
+	return (sum);
+
+}
