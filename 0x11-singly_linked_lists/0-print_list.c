e03ab45252fcac226a56e151f76ef4c932f699c3
Austin Harshberger <aharshbe@live.com>
2018-03-01 12:31:10 -0800

    adding number 0 for checker

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_list - print linked list
+ * @h: linked list to be operated on
+ *
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+	int num_nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d]", h->len);
+			printf("%s", h->str);
+
+		}
+		printf("\n");
+		h = h->next;
+		num_nodes++;
+	}
+	return (num_nodes);
+}
