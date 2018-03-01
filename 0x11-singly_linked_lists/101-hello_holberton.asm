52ad3a3cd775ceba86ec87dd0dd84b4ca46c2d04
Austin Harshberger <aharshbe@live.com>
2018-03-01 15:25:59 -0800

    adding fix to 101 with error haneling

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,15 @@
-section functions
+section function_printf
 	extern printf
 	section .to_print
-to_print: db "Hello, Holberton", 10, 0
+to_print: db "Hello, Holberton", 10
 
 	section .text
 	global main
 main:
 	push rbp
 	mov rdi, to_print
+	mov rax, 0
 	call printf
 	pop rbp
+	mov rax, 0
 	ret
