b1101b1514864747c11befcf6697add56c663d77
Austin Harshberger <aharshbe@live.com>
2018-03-08 15:39:31 -0800

    adding number 5

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - clear the bit
+ * @n: number to print
+ * @m: second number
+ * Return: 0
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count = 0, total = n ^ m;
+
+	while (total)
+	{
+		count += total & 1;
+		total >>= 1;
+	}
+	return (count);
+}
+
