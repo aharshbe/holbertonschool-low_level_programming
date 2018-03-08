068bfd8bddd56debfa9221730864b049f7b293bb
Austin Harshberger <aharshbe@live.com>
2018-03-08 15:25:43 -0800

    adding number 4

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- * set_bit - return the value of a bit at a given index
+ * clear_bit - clear the bit
  * @n: number to print
  * @index: index to obtain
  * Return: 0
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	long int shift = 1;
 
@@ -16,8 +16,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if ((sizeof(long int) * 8) < index)
 		return (-1);
 
-	shift <<= index;
-	*n |= shift;
-	
+	*n &= ~(shift <<= index);
 	return (1);
 }
