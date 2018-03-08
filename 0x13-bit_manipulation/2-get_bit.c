1b0ba55c4e0ccb113f7ea6944458b5b6726fb8fa
Austin Harshberger <aharshbe@live.com>
2018-03-08 14:54:29 -0800

    adding number 2

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * get_bit - return the value of a bit at a given index
+ * @n: number to print
+ * @index: index to obtain
+ * Return: 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > n)
+		return (-1);
+	return ((n >> index) & 1);
+}
