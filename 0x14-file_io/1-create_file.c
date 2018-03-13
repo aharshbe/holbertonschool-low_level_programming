e58fa89486c76b54ce6946890f0ef5cd4f0dcc6a
Austin <aharshbe@live.com>
2018-03-13 13:54:30 -0700

    adding edge cases to return success if the size of 0 or the string is NULL, and truncate the file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -18,7 +18,10 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[size])
 		size++;
 
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	if (!size)
+		return (success);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == fail)
 		return (fail);
