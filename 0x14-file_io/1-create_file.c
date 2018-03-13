c1f8d163a187439e027a0f3c49b775b088d3d7d5
Austin <aharshbe@live.com>
2018-03-13 13:55:36 -0700

    adding actual null check for content instead of using size

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,11 +15,11 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (fail);
 
+	if (!text_content)
+		return (success);
+
 	while (text_content[size])
 		size++;
-
-	if (!size)
-		return (success);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
