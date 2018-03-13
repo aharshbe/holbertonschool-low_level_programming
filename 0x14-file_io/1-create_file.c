9e7966f7ae30b550bf4d86337043db948e8eb606
Austin <aharshbe@live.com>
2018-03-13 14:02:47 -0700

    moving text to be after open

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,16 +14,16 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (fail);
 
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fd == fail)
+		return (fail);
+
 	if (!text_content)
 		return (success);
 
 	while (text_content[size])
 		size++;
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (fd == fail)
-		return (fail);
 
 	ot = write(fd, text_content, size);
 
