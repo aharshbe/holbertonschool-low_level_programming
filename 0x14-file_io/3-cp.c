039d07c59c7f5f45a7943f73c0d1cddb18eadc1d
Austin <aharshbe@live.com>
2018-03-13 16:43:54 -0700

    adding some changes to the logic for the argc check and moving around some declarations

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -12,86 +12,84 @@ int main(int argc, char **argv)
 	int open_ft, open_ff, fail = -1, write_to, rd, close_ff, close_ft;
 	char *buf, *file_to, *file_from;
 
-	if (argc == 3)
-	{
-		file_from = argv[1];
-		file_to = argv[2];
-
-		if (!file_from)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
-
-		if (!file_to)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_to);
-			exit(99);
-		}
-
-		open_ff = open(file_from, O_RDONLY);
-
-		if (open_ff == fail)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
-
-		open_ft = open(file_to, O_CREAT | O_RDWR | O_TRUNC, 0664);
-
-		if (open_ft == fail)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
-
-		buf = malloc(BUFFSIZE);
-
-		if (!buf)
-			return (0);
-
-		while ((rd = read(open_ff, buf, BUFFSIZE)))
-		{
-
-			if (rd == fail)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-				exit(98);
-			}
-			else
-			{
-				write_to = write(open_ft, buf, BUFFSIZE);
-
-				if (write_to == fail)
-				{
-					dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-					exit(99);
-				}
-			}
-		}
-		close_ff = close(open_ff);
-
-		if (close_ff == fail)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", open_ff);
-			exit(100);
-		}
-
-		close_ft = close(open_ft);
-
-		if (close_ft == fail)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", open_ft);
-			exit(100);
-		}
-
-		free(buf);
-	}
-	else
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
+	file_from = argv[1];
+	file_to = argv[2];
+
+	if (!file_from)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	if (!file_to)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_to);
+		exit(99);
+	}
+
+	open_ff = open(file_from, O_RDONLY);
+
+	if (open_ff == fail)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	open_ft = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (open_ft == fail)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	buf = malloc(BUFFSIZE);
+
+	if (!buf)
+		return (0);
+
+	while ((rd = read(open_ff, buf, BUFFSIZE)))
+	{
+
+		if (rd == fail)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			exit(98);
+		}
+
+		write_to = write(open_ft, buf, BUFFSIZE);
+
+		if (write_to == fail)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+
+	close_ff = close(open_ff);
+
+	if (close_ff == fail)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", open_ff);
+		exit(100);
+	}
+
+	close_ft = close(open_ft);
+
+	if (close_ft == fail)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", open_ft);
+		exit(100);
+	}
+
+	free(buf);
 	return (0);
 }
+
+
