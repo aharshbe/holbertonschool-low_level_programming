cbe97163671cb06b43c1814c0aefb4dba13fd15f
Austin <aharshbe@live.com>
2018-04-14 13:22:16 -0700

    adding number 8

---
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete node at given index
+ * @head: head of ddl list
+ * @index: index to insert at
+ * Return: 1 if success -1 if fail
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *old;
+	size_t i = 0;
+
+	if (!head || !head->next)
+		return (-1);
+	old = *head;
+
+	if (!index)
+	{
+		*head = old->next;
+		if (!(!*head))
+			(*head)->prev = NULL;
+		free(old);
+		return (1);
+	}
+
+	for ( ; head && i < index; old = old->next, i++)
+		;
+	if (!old)
+		return (-1);
+	old->prev->next = old->next;
+
+	if (!(!old->next))
+		old->next->prev = old->prev;
+	free(old);
+	return (1);
+}
