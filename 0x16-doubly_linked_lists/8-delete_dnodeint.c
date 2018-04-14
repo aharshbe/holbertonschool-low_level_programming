b004a14f4e75457e157d1156d4847b65d147ffdf
Austin <aharshbe@live.com>
2018-04-14 13:32:34 -0700

    adding changes to 9

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -23,7 +23,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for ( ; head && i < index; old = old->next, i++)
+	for ( ; head && old && i < index; old = old->next, i++)
 		;
 	if (!old || index > i)
 		return (-1);
