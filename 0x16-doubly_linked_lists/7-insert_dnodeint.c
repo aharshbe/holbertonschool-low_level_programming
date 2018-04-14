0b326d9302c76d78faa5c787e04a394ccfbb9886
Austin <aharshbe@live.com>
2018-04-14 13:09:07 -0700

    adding number 7

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,32 +9,38 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
-	size_t i = 1;
+	size_t i = 0;
 
 	if (!h)
 		return (NULL);
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
-
-	if (!idx || !*h)
+	if (!idx)
 	{
 		new = add_dnodeint(h, n);
 		return (new);
 	}
 
-	for ( ; *h; *h = (*h)->next, i++)
+	for ( ; *h && i < idx; i++, *h = (*h)->next)
+		;
+
+	if (!h && i == idx)
 	{
-		if ((*h)->next && i == idx)
-		{
-			new->prev = *h;
-			new->next = (*h)->next;
-			(*h)->next = new;
-			return (new);
-		}
+		new = add_dnodeint_end(h, n);
+		return (new);
 	}
-	new = add_dnodeint_end(h, n);
-	return (new);
+
+	if (!(!*h))
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (!new)
+			return (NULL);
+		new->n = n;
+		(*h)->prev->next = new;
+		new->prev = (*h)->prev;
+		(*h)->prev = new;
+		new->next = *h;
+		return (new);
+	}
+
+	return (NULL);
 }
