5cb4eebbe89c81cdf710402193c35b7c2eafbbe7
Austin Harshberger <aharshbe@live.com>
2018-04-18 14:38:24 -0700

    fixing PELOAD to PRELOAD

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -q -O /tmp/lib.so https://github.com/aharshbe/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/lib.so
-export LD_PELOAD=/tmp/lib.so
+export LD_PRELOAD=/tmp/lib.so
