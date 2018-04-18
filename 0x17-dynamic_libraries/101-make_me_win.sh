17dd3ce6fff442cfb78039f6ab6dab1c22be958a
Austin Harshberger <aharshbe@live.com>
2018-04-18 14:07:58 -0700

    adding 101

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#wget -qO /./u.so 
-export LD_PELOAD=/./u.so 
+wget -qO https://github.com/aharshbe/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/u.so
+export LD_PELOAD=$PWD/u.so
