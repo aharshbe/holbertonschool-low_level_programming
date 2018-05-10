7102b83aca1b2d14a4b78d1d641e4704a2bc1f36
Austin Harshberger <aharshbe@live.com>
2018-05-10 14:31:21 -0700

    adding the first 3 problems

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - create a hash
+ * @size: The size of the array
+ * Return: the hash table that was created
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *nh = NULL;
---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - provde key at given index
+ * @key: the key
+ * @size: the size of the arrat
+ * Return: the index at which the key/value pair was stored
+ */
+
+ unsigned long int key_index(const unsigned char *key, unsigned long int size)
+ {
+ 	(void)size;
+ 	return (hash_djb2(key) % size);
+ }
