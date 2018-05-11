eb47ab9f883e2a2e216b39e5905055c0005bc993
Austin <aharshbe@live.com>
2018-05-11 13:41:17 -0700

    adding changes to 0 again

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -7,28 +7,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned int i = 0;
 	hash_table_t *new_hash = 0;
 
-	if (size > 0)
-	{
-		new_hash = malloc(sizeof(hash_table_t));
-		if (new_hash)
-		{
-			new_hash->array = malloc(sizeof(hash_node_t) * size);
-			if (!new_hash)
-			{
-				free(new_hash);
-				return (NULL);
-			}
-		}
-		else
-			return (NULL);
-	}
-	else
+	if (!(size > 0))
 		return (NULL);
-	for ( ; i < size; i++)
-		new_hash->array[i] = 0;
+
+	new_hash = malloc(sizeof(hash_table_t));
+	if (!new_hash)
+		return (NULL);
+
+	new_hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_hash->array)
+		return (NULL);
+
 	new_hash->size = size;
+
 	return (new_hash);
 }
