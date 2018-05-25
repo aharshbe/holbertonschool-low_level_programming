ab5a4e70be014810e0065f59f7068472f4742ac3
austin <aharshbe@live.com>
2018-05-24 18:57:40 -0700

    adding initial stuff for quick sort

---
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts a list using quick sort
+ *
+ * @array: The list to be sorted
+ * @size: The size of the list
+ */
+void quick_sort(int *array, size_t size)
+{
+
+}
